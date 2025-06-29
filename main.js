0b8ed4135e4dd908b8c901fcca3626fcb25566f1
Mark <kowalewskimark25@gmail.com>
2025-06-29 19:55:21 -0400

    update Book constructor to include author, number of pages, and finished status

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const myLibrary = []
 
-function Book(title){
+function Book(title, author, numberOfPages, finished){
     if (!new.target) {
         throw Error("you must use the 'new' operator to make a new book")
     }
     this.title = title;
+    this.author = author
+    this.numberOfPages = numberOfPages
+    this.finished = finished
     this.id = crypto.randomUUID();
 }
 
